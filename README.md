1dbf302789fcdb97e78a21a0d21a63052ef656c3
Emil Cieslar <emil.cieslar@gmail.com>
2015-11-02 10:17:47 +0100

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# PHP-File-Multiupload
-File Multiupload class
+# PHP File Multiupload
+PHP Class that facilitates multiple file upload
+
+## GETTING STARTED
+1. **Copy Upload.php to you project directory**
+2. **require_once('Upload.php'); in the file that handles the upload**
+3. **Upload the file using: Upload::upload('github/PHP-File-Multiupload/upload');**
+4. **And that's it!**
+
+## MANUAL
--- Upload.php
+++ Upload.php
@@ -1,66 +1,108 @@
 <?php
 
-public class Upload
+/**
+ * Upload class
+ *
+ */
+class Upload
 {
-  public function upload()
-  {
-    $valid_formats = array("jpg", "png", "gif", "zip", "bmp");
-      $max_file_size = 1024*1000*2; //2000 kb
-      $path = $_SERVER['DOCUMENT_ROOT'] . substr(SITE_URL,1) . "upload/"; // Upload directory
-      $count = 0;
-
-      # Get if it's komiks being uploaded
-      $komiks = (isset($_POST['type']) && $_POST['type'] == "1") ? true : false;
-
-      # Loop $_FILES to exeicute all files
-    	foreach ($_FILES['files']['name'] as $f => $name) {
-
-    	    if ($_FILES['files']['error'][$f] == 4) {
-    	        continue; # Skip file if any error found
-    	    }
-
-    	    if ($_FILES['files']['error'][$f] == 0) {
-
-    	        if ($_FILES['files']['size'][$f] > $max_file_size)
-              {
-  	            $message[] = "$name is too large!.";
-  	            continue; # Skip large files
-
-    	        } elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $valid_formats) )
-              {
-        				$message[] = "$name is not a valid format";
-        				continue; # Skip invalid file formats
-
-        			} else
-              { # No error found! Move uploaded files
-                # Create unique name
-                $uniqueName = md5(uniqid(rand(), true));
-                $uniqueName .= "." . pathinfo($name, PATHINFO_EXTENSION);
-
-                # Get album ID
-                $albumId = $_POST['album'];
-
-                # Move uploaded file
-  	            if(move_uploaded_file($_FILES["files"]["tmp_name"][$f], $path.$uniqueName))
-  	            $count++; # Number of successfully uploaded file
-
-                # Resize image
-                $resizedImg = new Imagick($path.$uniqueName);
-                $resizedImg->cropThumbnailImage(180,180);
-                $resizedImg->writeImage($path."th_".$uniqueName);
-                $resizedImg->destroy();
-
-                # Save image
-                $image = $this->model('Image');
-                $image->setName($name);
-                $image->setFilename($uniqueName);
-                # If it's komiks image
-                if($komiks)
-                  $image->setType(1);
-                $image->setAlbumId($albumId);
-                $image->Save();
-    	        }
-    	    }
-    	}
+  public function __construct() {
+    self::upload();
   }
+
+  /**
+   * Function upload
+   *
+   * Uploads all files from $_FILES array
+   * @param String $dir the directory where files should be uploaded
+   * @param String[] $validFormats the array containing allowed formats
+   * @param int $maxFileSize maximum file size in MBs
+   * @param String $root root to the upload directory
+   *
+   * @return String[] contains 'error' and 'files' fields:
+   *                  'error' are any error messages
+   *                  'files' are successfully uploaded files
+   */
+  public static function upload($dir = 'upload',
+                                $validFormats = array("jpg", "png", "gif", "zip", "bmp"),
+                                $maxFileSize = 2,
+                                $root = '')
+  {
+    // Set path for the upload directory
+    if(empty($root))
+      $path = $_SERVER['DOCUMENT_ROOT'] . '/' . $dir . '/';
+    else
+      $path = $root . '/' . $dir . '/';
+
+    // Set maximum files size in MB
+    $maxFileSize *= 1024*1000;
+
+    // Array that will contain names of uploaded files
+    $uploadedFiles = [];
+    // Array that will contain error messages if any
+    $message = [];
+
+    // Loop $_FILES to go over all files
+    foreach ($_FILES['files']['name'] as $f => $name)
+    {
+      // Check for any errors
+      // You can read up more about errors when uploading files here: TODO: Add link to errors explained in PHP manual
+      if ($_FILES['files']['error'][$f] != 0)
+      {
+        switch($_FILES['files']['error'][$f])
+        {
+          case UPLOAD_ERR_INI_SIZE:
+            $message[] = "$name is too large!.";
+            break;
+          case UPLOAD_ERR_FORM_SIZE:
+          case UPLOAD_ERR_PARTIAL:
+          case UPLOAD_ERR_NO_FILE:
+          case UPLOAD_ERR_CANT_WRITE:
+            $message[] = "Cannot write to disk";
+            break;
+          default:
+        }
+        continue; // Skip file if any error found
+      }
+
+      // If no errors are found, continue
+      if ($_FILES['files']['error'][$f] == 0)
+      {
+        // Check if file is not too large
+        if ($_FILES['files']['size'][$f] > $maxFileSize)
+        {
+          $message[] = "$name is too large!.";
+          continue; // Skip large files
+        }
+        // Check if it's allowed file format
+        elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $validFormats) )
+        {
+          $message[] = "$name is not a valid format";
+          continue; // Skip invalid file formats
+
+        }
+        // No error found! Move uploaded files
+        else
+        {
+          // Create unique name
+          $uniqueName = md5(uniqid(rand(), true));
+          // Add file extension to the name
+          $uniqueName .= "." . pathinfo($name, PATHINFO_EXTENSION);
+
+          // Move the uploaded file to its final destination
+          if(move_uploaded_file($_FILES["files"]["tmp_name"][$f], $path.$uniqueName))
+
+          // Add the file to the array
+          $uploadedFiles[] = $uniqueName;
+        }
+
+      } // No errors
+    } // Foreach
+
+    // return String[] contains 'error' and 'files' fields:
+    // 1. 'error' are any error messages
+    // 2. 'files' are successfully uploaded files
+    return array('errors'=>$message,'files'=>$uploadedFiles);
+  }
+
 }
