c0654f9a1de85ed1f877b47b0e40ed360d7409c6
Emil Cieslar <emilcieslar@Emils-MacBook-Pro.local>
2015-11-09 21:24:24 +0100

    New version of upload class

--- test.php
+++ test.php
@@ -1,13 +1,19 @@
 <?php
 
-# Display errors for testing purposes
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
 // This class is needed for upload
 require_once('Upload.php');
-// Trigger the upload method
-$upload = Upload::upload('github/PHP-File-Multiupload/upload');
-// Print the results
-print_r($upload);
+// Create the instance and pass in the files array
+$upload = new Upload($_FILES['files']);
+// Set the upload directory
+$upload->setDir('github/PHP-File-Multiupload/upload');
+
+// Upload the files
+$upload->upload();
+
+// Get error messages if any
+if($errors = $upload->getErrors())
+  print_r($errors);
+
+// Get names of uploaded files
+if($files = $upload->getUploadedFilesNames())
+  print_r($files);
